eee7844c9ad57c58b7fcad23ca939d1fc6e606be
Daniel Cabral <danicabral15@outlook.com>
2024-02-03 01:42:54 -0300

    add GameDetailCard

--- apps/frontend/src/components/games/gameshome/card/GameCard.jsx
+++ apps/frontend/src/components/games/gameshome/card/GameCard.jsx
@@ -2,37 +2,18 @@ import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import "./GameCard.css";
 import { GameCardRating } from "./rating/GameCardRating";
+import { GameDetailCard } from "./detailcard/GameDetailCard";
 
 export const GameCard = ({ game }) => {
   return (
     <Link key={game.id} to={`/games/gamepage/${game.id}`}>
       <div className="flex items-center justify-between p-3 my-5 border rounded-lg shadow-lg cursor-pointer bg-base-300 border-base-content border-opacity-20">
-        <div className="flex flex-1 gap-x-3">
-          <img
-            src={game.cover.url.replace("t_thumb", "t_1080p")}
-            alt={`${game.name} cover`}
-            className="object-cover w-20 h-full md:w-36 md:h-full"
-          />
-          <div className="flex-1">
-            <h2 className="font-bold text-balance line-clamp-1 text-base-content">
-              {game.name}
-            </h2>
-            <ul className="flex justify-start gap-4 my-1">
-              {game.platforms
-                .filter((platform) => platform.abbreviation != null)
-                .map((platform) => {
-                  return (
-                    <li key={platform.id} className="text-error">
-                      {platform.abbreviation}
-                    </li>
-                  );
-                })}
-            </ul>
-            <p className="line-clamp-2 lg:line-clamp-3 text-base-content">
-              {game.summary}
-            </p>
-          </div>
-        </div>
+        <GameDetailCard
+          cover={game.cover}
+          name={game.name}
+          platforms={game.platforms}
+          summary={game.summary}
+        />
         <GameCardRating rating={game.rating} />
       </div>
     </Link>
