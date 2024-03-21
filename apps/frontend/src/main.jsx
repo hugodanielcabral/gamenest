032c7e2c632ee32110d6d96650886ebd46357418
Daniel Cabral <danicabral15@outlook.com>
2024-03-20 22:06:20 -0300

    Add Roboto font package

--- apps/frontend/src/main.jsx
+++ apps/frontend/src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import { GameNestApp } from "./GameNestApp";
 import "./index.css";
+import "@fontsource/roboto";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
