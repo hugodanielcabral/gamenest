ead87ce665517dd6bb9c1f6adfddf3afeea9dbcc
Daniel Cabral <danicabral15@outlook.com>
2024-03-23 13:36:19 -0300

    Refactor GameDetailsHeader and HeaderRating components

--- apps/frontend/src/components/games/gamedetails/header/rating/HeaderRating.jsx
+++ apps/frontend/src/components/games/gamedetails/header/rating/HeaderRating.jsx
@@ -1,20 +1,25 @@
 import propTypes from "prop-types";
 import { GameCardRating } from "../../../gameshome/card/rating/GameCardRating";
+import { Button } from "../../../../ui/index.js";
+import { useNavigate } from "react-router-dom";
 
-export const HeaderRating = ({ rating }) => {
+export const HeaderRating = ({ rating, id }) => {
+  const navigate = useNavigate();
   return (
     <div className="flex flex-col items-center col-span-5 gap-y-3 md:justify-evenly md:col-span-1">
       <GameCardRating rating={rating} />
-      <button
-        className="text-base font-bold btn btn-warning hover:btn-warning/50 disabled:bg-stone-500 disabled:cursor-not-allowed"
-        disabled
+
+      <Button
+        onClick={() => navigate(`/collection/add/${id}`)}
+        className="text-lg"
       >
-        Add to the collection
-      </button>
+        Add to collection
+      </Button>
     </div>
   );
 };
 
 HeaderRating.propTypes = {
   rating: propTypes.number.isRequired,
+  id: propTypes.number.isRequired,
 };
