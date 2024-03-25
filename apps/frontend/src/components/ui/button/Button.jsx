40167b63fd456415f166819df40fb9cac874fb6b
Daniel Cabral <danicabral15@outlook.com>
2024-03-24 21:07:34 -0300

    Update button styles

--- apps/frontend/src/components/ui/button/Button.jsx
+++ apps/frontend/src/components/ui/button/Button.jsx
@@ -7,7 +7,7 @@ export const Button = ({ children, className, ...props }) => {
     <button
       className={twMerge(
         clsx(
-          "inline-block rounded bg-indigo-600 px-8 py-3 text-sm font-medium text-white transition hover:-rotate-2 hover:scale-110 focus:outline-none focus:ring active:bg-indigo-500",
+          "inline-block rounded bg-buttons-500 px-8 py-3 text-sm font-medium text-textWhite-500 transition hover:scale-110 focus:outline-none focus:ring active:bg-buttons-400",
           className
         )
       )}
