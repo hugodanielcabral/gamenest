f0de8b1745cff821d1ff4725dd29f601c901d2b5
Daniel Cabral <danicabral15@outlook.com>
2024-04-03 17:41:22 -0300

    Delete unused gallery components

--- apps/frontend/src/components/games/gamedetails/gallery/index.js
+++
@@ -1,5 +0,0 @@
-import { GalleryScreenshots } from "./screenshots/GalleryScreenshots";
-import { GalleryArtworks } from "./artworks/GalleryArtworks";
-import { GalleryVideos } from "./videos/GalleryVideos";
-
-export { GalleryScreenshots, GalleryArtworks, GalleryVideos };
