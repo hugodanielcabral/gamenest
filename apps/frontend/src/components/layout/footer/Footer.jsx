0c27885a97bc41179e3ab73ecac6342a280c9c8b
Daniel Cabral <danicabral15@outlook.com>
2024-04-15 16:58:31 -0300

    Refactor UI components and add new components for social links in Footer.jsx and mySocialLinks.js

--- apps/frontend/src/components/layout/footer/Footer.jsx
+++ apps/frontend/src/components/layout/footer/Footer.jsx
@@ -1,61 +1,37 @@
+import gameNestLogo2 from "../../../assets/logos/gamenest-logo-2.png";
+import { mySocialLinks } from "../../../utils/mySocialLinks";
+
 export const Footer = () => {
   return (
-    <footer className="p-10 text-pink-500 rounded footer bg-base-300 footer-center">
-      <nav className="grid grid-flow-col gap-4">
-        <a
-          href="https://danielcabral.onrender.com/"
-          target="_blank"
-          rel="noreferrer"
-          className="link link-hover"
-        >
-          About me
-        </a>
-        <a className="link link-hover">Contact</a>
-      </nav>
-      <nav>
-        <div className="grid grid-flow-col gap-4">
-          <a>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              className="fill-current"
-            >
-              <path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"></path>
-            </svg>
-          </a>
-          <a>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              className="fill-current"
-            >
-              <path d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
-            </svg>
-          </a>
-          <a>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              className="fill-current"
-            >
-              <path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
-            </svg>
-          </a>
+    <footer className="bg-base-300">
+      <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 lg:px-8">
+        <div className="sm:flex sm:items-center sm:justify-between">
+          <div className="flex justify-center text-teal-600 sm:justify-start">
+            <img
+              src={gameNestLogo2}
+              alt="GameNest Logo"
+              className="h-14 w-auto"
+            />
+          </div>
+
+          <ul className="flex gap-2 justify-center items-center">
+            {mySocialLinks.map((socialLink) => (
+              <li
+                key={socialLink.id}
+                className="hover:scale-110 transition-transform duration-300 ease-in-out"
+              >
+                <a href={socialLink.url} target="_blank" rel="noreferrer">
+                  <img
+                    src={socialLink.icon}
+                    alt={socialLink.name}
+                    className="size-8 md:size-11"
+                  />
+                </a>
+              </li>
+            ))}
+          </ul>
         </div>
-      </nav>
-      <aside>
-        <p>
-          Copyright © 2024 - All right reserved by
-          <span className="font-bold text-base-content"> Game</span>
-          <span className="font-bold text-error">Nest</span>
-        </p>
-      </aside>
+      </div>
     </footer>
   );
 };
---
+++ apps/frontend/src/utils/mySocialLinks.js
@@ -0,0 +1,24 @@
+import linkedinIcon from "../assets/icons/linkedin.webp";
+import githubIcon from "../assets/icons/github.webp";
+import portfolioIcon from "../assets/icons/portfolio.webp";
+
+export const mySocialLinks = [
+  {
+    id: 1,
+    name: "LinkedIn",
+    url: "https://www.linkedin.com/in/hugo-daniel-cabral",
+    icon: linkedinIcon,
+  },
+  {
+    id: 2,
+    name: "GitHub",
+    url: "https://github.com/hugodanielcabral",
+    icon: githubIcon,
+  },
+  {
+    id: 3,
+    name: "Portfolio",
+    url: "https://danielcabral.dev.ar/",
+    icon: portfolioIcon,
+  },
+];
