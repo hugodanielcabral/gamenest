945c02380c0ad68f40b810887911eb2ab6e6b1a1
Daniel Cabral <danicabral15@outlook.com>
2024-07-20 17:29:29 -0300

    refactor: Improve GameDetailsSkeleton component styles and structure

--- apps/frontend/src/components/games/gamedetails/skeleton/GameDetailsSkeleton.jsx
+++ apps/frontend/src/components/games/gamedetails/skeleton/GameDetailsSkeleton.jsx
@@ -2,35 +2,28 @@ import Skeleton from "react-loading-skeleton";
 
 export const GameDetailsSkeleton = () => {
   return (
-    <div className="min-h-screen container mx-auto mt-16">
+    <div className="container mx-auto mt-16 min-h-screen p-4">
       {/* Header */}
-      <div className="flex md:flex-row flex-col items-center">
+      <div className="flex items-center">
         <div className="flex-grow">
-          <Skeleton height={50} width={200} count={1} />
-        </div>
-        <div className="flex-grow">
-          <Skeleton height={50} width={200} count={1} />
+          <Skeleton height={50} count={2} />
         </div>
       </div>
 
       {/* Cover, Trailer, Add to Collection button, Brief description and Tabs */}
       <div className="grid grid-cols-8 gap-3">
-        <div className="col-span-8 md:col-span-2">
+        <div className="col-span-8 lg:col-span-2">
           <Skeleton height={500} />
           <Skeleton height={50} />
         </div>
-        <div className="col-span-8 md:col-span-6">
+        <div className="col-span-8 lg:col-span-4">
           <Skeleton height={500} />
           <Skeleton height={50} />
         </div>
-      </div>
 
-      {/* Screenshots, Videos, Artworks and Additional info */}
-      <div className="my-2 mx-auto grid grid-cols-2 md:grid-cols-4 gap-3">
-        <Skeleton height={50} />
-        <Skeleton height={50} />
-        <Skeleton height={50} />
-        <Skeleton height={50} />
+        <div className="col-span-8 lg:col-span-2">
+          <Skeleton height={550} />
+        </div>
       </div>
     </div>
   );
