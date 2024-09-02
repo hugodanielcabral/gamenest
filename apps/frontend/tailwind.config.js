954c1b875ef3719f3673472dbe4e898d307562e7
Daniel Cabral <danicabral15@outlook.com>
2024-09-02 14:32:22 -0300

    chore: Update npm dependencies for @fontsource/nunito to version 5.0.19

--- apps/frontend/tailwind.config.js
+++ apps/frontend/tailwind.config.js
@@ -8,6 +8,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        nunito: ["Nunito", "sans-serif"],
+      },
       variants: {
         extend: {
           opacity: ["disabled"],
