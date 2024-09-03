e4ca33f83f4dc6664c324c5e7cb00146e7008d03
Daniel Cabral <danicabral15@outlook.com>
2024-09-03 12:40:23 -0300

    Refactor: Updated Card components to include className prop

--- apps/frontend/src/components/ui/card/Card.tsx
+++ apps/frontend/src/components/ui/card/Card.tsx
@@ -1,30 +1,44 @@
+import clsx from "clsx";
+import { twMerge } from "tailwind-merge";
+
 interface CardProps {
   imgSrc?: string | Function;
   title?: string;
   children?: React.ReactNode;
+  className?: string;
 }
 
-const CardImage = ({ imgSrc, title }: CardProps) => {
+const CardImage = ({ imgSrc, className, title }: CardProps) => {
   return (
     <img
       src={typeof imgSrc === "function" ? imgSrc() : imgSrc}
       alt={title}
-      className="rounded-md border-2 border-gray-700"
+      className={twMerge(
+        clsx("h-full rounded-md border-2 border-gray-700", className),
+      )}
     />
   );
 };
 
-const CardContent = ({ title, children }: CardProps) => {
+const CardContent = ({ className, children }: CardProps) => {
   return (
-    <div className="card-content absolute bottom-0 z-10 flex h-full w-full flex-col items-center justify-center rounded-md border-2 bg-base-100 bg-opacity-0 opacity-0 transition-all duration-300 ease-in-out group-hover:border-gray-500 group-hover:bg-opacity-90 group-hover:opacity-100">
-      <h2 className="card-title text-pretty text-xl text-info">{title}</h2>
+    <div
+      className={twMerge(
+        clsx(
+          "card-content absolute bottom-0 z-10 flex h-full w-full flex-col items-center justify-center rounded-md border-2 bg-base-100 bg-opacity-0 opacity-0 transition-all duration-300 ease-in-out group-hover:border-gray-500 group-hover:bg-opacity-90 group-hover:opacity-100",
+          className,
+        ),
+      )}
+    >
       {children}
     </div>
   );
 };
 
-const Card = ({ children }: CardProps) => {
-  return <div className="group relative">{children}</div>;
+const Card = ({ className, children }: CardProps) => {
+  return (
+    <div className={twMerge(clsx("group relative", className))}>{children}</div>
+  );
 };
 
 export { Card, CardImage, CardContent };
