81e0ba7734d019f7f68b3ce8fdeee8ca3d4af920
Daniel Cabral <danicabral15@outlook.com>
2024-09-03 12:41:40 -0300

    refactor: Changed Carousel to a Grid columns

--- apps/frontend/src/components/home/connected/popular/HomePopularGames.tsx
+++ apps/frontend/src/components/home/connected/popular/HomePopularGames.tsx
@@ -4,11 +4,6 @@ import { Link } from "react-router-dom";
 import { FaExternalLinkAlt, FaStar, FaFire } from "react-icons/fa";
 import { Card, CardContent, CardImage } from "../../../ui/card/Card";
 import getImageUrl from "../../../../utils/getImageUrl";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { A11y, Navigation, Pagination } from "swiper/modules";
-import "swiper/css";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
 
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 
@@ -27,42 +22,50 @@ export const HomePopularGames = () => {
     `${BASE_URL}/popular/games`,
   );
 
+  const getTopGamesByRating = (popularGames: PopularGame[]) => {
+    return popularGames.slice(0, 5).sort((a, b) => {
+      if (a.rating > b.rating) {
+        return -1;
+      }
+      if (a.rating < b.rating) {
+        return 1;
+      }
+
+      return 0;
+    });
+  };
+
   return (
-    <section className="flex flex-col justify-center items-center gap-6">
+    <section className="flex flex-col items-center justify-center gap-8">
       <h2 className="text-3xl text-gray-300">
         Juegos populares <FaFire className="inline text-red-500" />{" "}
       </h2>
       {!isLoading ? (
-        <>
-          <Swiper
-            modules={[Navigation, Pagination, A11y]}
-            spaceBetween={0}
-            slidesPerView={3}
-            className="max-w-5xl"
-            navigation
-            pagination={{ clickable: true }}
-          >
-            {popularGamesData?.map((game: PopularGame) => (
-              <SwiperSlide key={game.id} className="px-10">
-                <Card>
-                  <CardImage
-                    imgSrc={() => getImageUrl(game.cover.url, "cover_big_2x")}
-                    title={game.name}
-                  />
-                  <CardContent title={game.name}>
-                    <p className="absolute left-2 right-2 top-28 z-10 flex flex-col items-center gap-1 text-lg text-gray-300">
-                      <FaStar className="text-yellow-400" />
-                      {Math.floor(game.rating)} / 100
-                    </p>
-                    <Link to={`/games/${game.slug}`}>
-                      <FaExternalLinkAlt className="absolute right-2 top-2 z-10 size-6 text-gray-300 opacity-0 transition-colors duration-300 ease-in-out hover:text-info group-hover:opacity-100" />
-                    </Link>
-                  </CardContent>
-                </Card>
-              </SwiperSlide>
-            ))}
-          </Swiper>
-        </>
+        <div className="grid max-w-4xl grid-cols-4 gap-2 self-center">
+          {getTopGamesByRating(popularGamesData).map((game: PopularGame) => (
+            <Card
+              key={game.id}
+              className="col-span-4 row-span-1 h-80 sm:col-span-2 sm:h-full md:col-span-1 first:md:col-span-2 first:md:row-span-2"
+            >
+              <CardImage
+                imgSrc={() => getImageUrl(game.cover.url, "cover_big_2x")}
+                title={game.name}
+              />
+              <CardContent>
+                <h2 className="card-title text-pretty text-lg text-info md:text-sm group-first:md:text-xl">
+                  {game.name}
+                </h2>
+                <p className="absolute left-2 top-2 z-10 flex flex-col items-center gap-1 text-sm text-gray-300 md:text-lg">
+                  <FaStar className="text-sm text-yellow-400 md:text-lg" />
+                  {Math.floor(game.rating)} / 100
+                </p>
+                <Link to={`/games/${game.slug}`}>
+                  <FaExternalLinkAlt className="absolute right-2 top-2 z-10 size-6 text-gray-300 opacity-0 transition-colors duration-300 ease-in-out hover:text-info group-hover:opacity-100" />
+                </Link>
+              </CardContent>
+            </Card>
+          ))}
+        </div>
       ) : (
         <div className="mt-10">
           <Loading />
--- apps/frontend/src/components/layout/Layout.jsx
+++ apps/frontend/src/components/layout/Layout.jsx
@@ -1,5 +1,4 @@
 import propTypes from "prop-types";
-import { Navbar } from "../navbar/Navbar.jsx";
 import { Header } from "./header/Header.tsx";
 import { Footer } from "./footer/Footer.jsx";
 import { twMerge } from "tailwind-merge";
@@ -8,8 +7,6 @@ import clsx from "clsx";
 export const Layout = ({ children, className }) => {
   return (
     <>
-      {/*       <Navbar />
-       */}{" "}
       <Header />
       <main className={twMerge(clsx("min-h-screen", className))}>
         {children}
