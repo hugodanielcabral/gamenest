b0f3c0501f47368d54ba5541ef4349f51fea6071
Daniel Cabral <danicabral15@outlook.com>
2024-09-05 13:01:27 -0300

    refactor: Update GameDetailsHeader component and related files

--- apps/frontend/src/components/games/details/header/GameDetailsHeader.tsx
+++ apps/frontend/src/components/games/details/header/GameDetailsHeader.tsx
@@ -1,26 +1,13 @@
 import { platformsIcons } from "../../../../data/platformsIcons";
+import type { GameDetailsContentProps } from "../../../../types/gameDetails";
 import getImageUrl from "../../../../utils/getImageUrl";
 import { CollectionButton } from "../../../CollectionButton";
 import { Icon } from "../../../ui/icon/Icon";
 
-interface GameDetailsHeaderProps {
-  gameDetail: {
-    id: number;
-    name: string;
-    cover: { id: number; url: string };
-    platforms: { id: number; abbreviation: string; name: string }[];
-    screenshots: { id: number; url: string }[];
-  };
-  gameSlug: string;
-}
-
 export const GameDetailsHeader = ({
   gameDetail,
   gameSlug,
-}: GameDetailsHeaderProps) => {
-
-
-
+}: GameDetailsContentProps) => {
   return (
     <section className="relative flex flex-col gap-4 p-4 md:flex-row">
       <img
@@ -35,7 +22,7 @@ export const GameDetailsHeader = ({
         alt={`Cover del juego "${gameDetail.name}"`}
         className="z-10 h-full w-64 self-center rounded-md shadow-md shadow-black"
       />
-      <div className="z-10 mb-10 space-y-4 self-center  md:self-end">
+      <div className="z-10 mb-10 space-y-4 self-center md:self-end">
         <div className="flex gap-2">
           {gameDetail?.platforms.map((platform) => (
             <span
@@ -43,14 +30,17 @@ export const GameDetailsHeader = ({
               className="tooltip tooltip-top text-pretty"
               data-tip={platform.name}
             >
-              <Icon name={platformsIcons[platform.name]} className="size-6 md:size-8 hover:text-gray-300 text-gray-400" />
+              <Icon
+                name={platformsIcons[platform.name]}
+                className="size-6 text-gray-300 hover:text-gray-400 md:size-8"
+              />
             </span>
           ))}
         </div>
-        <h1 className="text-pretty text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-white">
+        <h1 className="text-pretty text-xl text-white md:text-2xl lg:text-3xl xl:text-4xl">
           {gameDetail?.name}
         </h1>
-       <CollectionButton gameSlug={gameSlug} />
+        <CollectionButton gameSlug={gameSlug} />
       </div>
     </section>
   );
---
+++ apps/frontend/src/types/gameDetails.ts
@@ -0,0 +1,52 @@
+interface GameDetailsProps {
+  data: {
+    id: number;
+    name: string;
+    cover: { id: number; url: string };
+    platforms: { id: number; abbreviation: string; name: string }[];
+    screenshots: { id: number; url: string }[];
+    videos: { id: number; video_id: string }[];
+    summary: string;
+    storyline: string;
+    genres: { id: number; name: string }[];
+    involved_companies: {
+      company: { id: number; name: string };
+      developer: boolean;
+    }[];
+    release_dates: {
+      id: number;
+      date: number;
+      game: { id: number; name: string };
+      platform: { id: number; name: string };
+    }[];
+  } | null;
+
+  isLoading: boolean;
+}
+
+interface GameDetailsContentProps {
+  gameDetail: {
+    id: number;
+    name: string;
+    cover: { id: number; url: string };
+    platforms: { id: number; abbreviation: string; name: string }[];
+    screenshots: { id: number; url: string }[];
+    videos: { id: number; video_id: string }[];
+    summary: string;
+    storyline: string;
+    genres: { id: number; name: string }[];
+    involved_companies: {
+      company: { id: number; name: string };
+      developer: boolean;
+    }[];
+    release_dates: {
+      id: number;
+      date: number;
+      game: { id: number; name: string };
+      platform: { id: number; name: string };
+    }[];
+  };
+  gameSlug?: string;
+}
+
+export type { GameDetailsProps, GameDetailsContentProps };
