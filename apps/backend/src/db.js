25ba651ec40f787cb56e500cb11cf02dc0004152
Daniel Cabral <danicabral15@outlook.com>
2024-09-05 19:09:26 -0300

    refactor: Update database connection configuration

--- apps/backend/src/db.js
+++ apps/backend/src/db.js
@@ -3,15 +3,19 @@ import dotenv from "dotenv";
 
 dotenv.config({ path: ".env" });
 
-const sql = postgres(process.env.DATABASE_URL, {
+const connectionString = process.env.DATABASE_URL;
+
+const sql = postgres(connectionString);
+
+/* const sql = postgres(process.env.DATABASE_URL, {
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  ssl: {
+   ssl: {
     rejectUnauthorized: true,
-  },
-});
+  }, 
+}) */
 
 export default sql;
