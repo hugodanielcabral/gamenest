c6085d00c060875d8bc37edf0539019539a06155
Daniel Cabral <danicabral15@outlook.com>
2024-09-12 10:35:14 -0300

    Add query-string library and update Loading component

---
+++ apps/frontend/src/components/ui/loading/Loading.tsx
@@ -0,0 +1,24 @@
+import clsx from "clsx";
+import { twMerge } from "tailwind-merge";
+
+interface LoadingProps {
+  className?: string;
+  color:
+    | "primary"
+    | "secondary | accent | neutral | success | warning | error | info";
+  type?: "spinner" | "dots" | "ring" | "ball" | "bars" | "infinity";
+}
+
+export const Loading = ({
+  color = "primary",
+  type,
+  className,
+}: LoadingProps) => {
+  return (
+    <div className={twMerge(clsx(className))}>
+      <span
+        className={`loading loading-lg mx-auto size-32 sm:size-36 md:size-40 lg:size-44 text-${color} loading-${type}`}
+      ></span>
+    </div>
+  );
+};
