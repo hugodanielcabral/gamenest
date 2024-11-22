044783ab59b98f77ac2da7a6a72de101076815a2
Daniel Cabral <danicabral15@outlook.com>
2024-11-22 18:36:43 -0300

    Add HomeLatest component to display latest released games in a carousel

---
+++ apps/frontend/src/components/home/latest/HomeLatest.tsx
@@ -0,0 +1,69 @@
+import { DateTime } from "luxon";
+import { useDataFetch } from "../../../hooks/useDataFetch";
+import { Card, CardContent, CardImage } from "../../ui/card/Card";
+import { Loading } from "../../ui/loading/Loading.tsx";
+import getImageUrl from "../../../utils/getImageUrl";
+import { Carousel, CarouselItem } from "../../ui/carousel/Carousel.tsx";
+
+interface HomeLatestProps {
+  fetchData: {
+    id: number;
+    name: string;
+    cover: {
+      id: number;
+      url: string;
+    };
+    first_release_date: number;
+    platforms: {
+      id: number;
+      abbreviation: string;
+      name: string;
+    }[];
+    slug: string;
+  }[];
+}
+
+export const HomeLatest = () => {
+  const { fetchData, isLoading } = useDataFetch<HomeLatestProps["fetchData"]>(
+    `games/latest/released`,
+  );
+
+  if (isLoading) {
+    return (
+      <Loading
+        className="flex min-h-screen items-start justify-center lg:col-span-3"
+        color="primary"
+        type="ring"
+      />
+    );
+  }
+
+  return (
+    <div className="w-full">
+      <Carousel>
+        {fetchData.map((game) => (
+          <CarouselItem>
+            <Card
+              className="w-40 sm:w-44 md:w-52 lg:w-64"
+              linkTo={`/games/${game.slug}`}
+            >
+              <CardImage
+                imgSrc={() => getImageUrl(game.cover.url, "cover_big_2x")}
+                title={game.name}
+                className="h-48 sm:h-60 md:h-72 lg:h-80 xl:h-96"
+              />
+              <CardContent>
+                <h3 className="line-clamp-1 text-pretty text-xs font-semibold sm:text-sm md:text-base">
+                  {game.name}
+                </h3>
+                <p className="text-base-content-secondary text-xs md:text-sm">
+                  {DateTime.fromSeconds(game.first_release_date).toFormat("DD")}
+                </p>
+              </CardContent>
+            </Card>
+          </CarouselItem>
+        ))}
+      </Carousel>
+    </div>
+  );
+};
--- apps/frontend/src/pages/HomePage.tsx
+++ apps/frontend/src/pages/HomePage.tsx
@@ -1,3 +1,4 @@
+import { HomeLatest } from "../components/home/latest/HomeLatest";
 import { HomePlatforms } from "../components/home/platforms/HomePlatforms";
 import { HomePopular } from "../components/home/popular/HomePopular";
 import { Layout } from "../components/layout/Layout";
@@ -6,9 +7,10 @@ import { Container } from "../components/ui/container/Container";
 const HomePage = () => {
   return (
     <Layout>
-      <Container className="flex flex-col space-y-4 md:space-y-8 p-4">
+      <Container className="flex flex-col space-y-4 p-4 md:space-y-8">
         <HomePopular />
         <HomePlatforms />
+        <HomeLatest />
       </Container>
     </Layout>
   );
