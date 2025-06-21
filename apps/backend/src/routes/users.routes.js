3c71070b454ae783786f949a6b66a819c28edf63
Daniel Cabral <danicabral15@outlook.com>
2025-06-21 18:17:54 -0300

    refactor: remove deprecated controllers, repositories, routes, and validation files

--- apps/backend/src/routes/users.routes.js
+++
@@ -1,9 +0,0 @@
-import Router from "express";
-import { AuthMiddleware } from "../middlewares/auth";
-import { getProfileStats } from "../controllers/users.controller.js";
-
-const router = Router();
-
-router.get("/user/profile/stats", AuthMiddleware.validateJWT, getProfileStats);
-
-export default router;
